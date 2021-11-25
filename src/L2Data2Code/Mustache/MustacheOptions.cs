e49a56c3077ef5cf8b34883f152df71c78846b52
topelab <joan@lopezlopez.cat>
2021-11-25 22:12:24 +0100

    Mustache actions

--- src/L2Data2Code/Mustache/MustacheOptions.cs
+++ src/L2Data2Code/Mustache/MustacheOptions.cs
@@ -1,3 +1,6 @@
+using L2Data2Code.BaseMustache.Interfaces;
+using L2Data2Code.SharedLib.Helpers;
+
 namespace Mustache
 {
     internal class MustacheOptions
@@ -6,5 +9,7 @@ namespace Mustache
         internal string TemplatePath { get; set; }
         internal string OutputPath { get; set; }
         internal string Collection { get; set; }
+        public IJsonSetting JsonSetting { get; internal set; }
+        public IMustacheRenderizer Mustacher { get; internal set; }
     }
 }
--- src/L2Data2Code/Mustache/Program.cs
+++ src/L2Data2Code/Mustache/Program.cs
@@ -11,51 +11,25 @@ namespace Mustache
 {
     class Program
     {
-        private static IMustacheRenderizer mustacher;
         static void Main(string[] args)
         {
             var options = MustacheOptionsFactory.Create(args);
-            IJsonSetting jsonSetting = new JsonSetting(options.JsonDataFile);
-            IMustacheHelpers helpers = new MustacheHelpers();
-            mustacher = new MustacheRenderizer(helpers);
+            options.JsonSetting = new JsonSetting(options.JsonDataFile);
+            options.Mustacher = new MustacheRenderizer(new MustacheHelpers());
 
             IFileExecutor fileExecutor = new FileExecutor(options.TemplatePath);
 
-            fileExecutor.Run(
-                (path) => CreatePaths(path, jsonSetting.Config, options.TemplatePath, options.OutputPath, options.Collection),
-                (file) => MustacheToOutput(file, jsonSetting.Config, options.TemplatePath, options.OutputPath, options.Collection));
+            fileExecutor.Run((path) => DoAction(CreatePath, path, options),
+                             (file) => DoAction(TransformFile, file, options));
         }
 
-        private static void CreatePaths(string path, JObject view, string sourcePath, string outputPath, string entity)
+        private static void DoAction(Action<string, string, string, IMustacheRenderizer, JToken> action, string path, MustacheOptions options)
         {
-            outputPath = outputPath.AddPathSeparator();
-            sourcePath = sourcePath.AddPathSeparator();
-            var entities = string.IsNullOrWhiteSpace(entity) ? view : view.SelectToken(entity);
-
-            if (entities != null)
-            {
-                if (!Directory.Exists(outputPath))
-                {
-                    Directory.CreateDirectory(outputPath);
-                }
-
-
-                foreach (var item in entities)
-                {
-                    string destPath = mustacher.Render(path.Replace(sourcePath, outputPath, StringComparison.CurrentCultureIgnoreCase), item);
-                    if (!Directory.Exists(destPath))
-                    {
-                        Directory.CreateDirectory(destPath);
-                    }
-                }
-            }
-
-        }
-
-        private static void MustacheToOutput(string file, JObject view, string sourcePath, string outputPath, string entity)
-        {
-            outputPath = outputPath.AddPathSeparator();
-            sourcePath = sourcePath.AddPathSeparator();
+            var outputPath = options.OutputPath.AddPathSeparator();
+            var sourcePath = options.TemplatePath.AddPathSeparator();
+            var view = options.JsonSetting.Config;
+            var entity = options.Collection;
+            var mustacher = options.Mustacher;
             var entities = string.IsNullOrWhiteSpace(entity) ? view : view.SelectToken(entity);
 
             if (entities != null)
@@ -67,12 +41,26 @@ namespace Mustache
 
                 foreach (var item in entities)
                 {
-                    string outpuFile = mustacher.Render(file.Replace(sourcePath, outputPath, StringComparison.CurrentCultureIgnoreCase), item);
-                    string text = mustacher.Render(File.ReadAllText(file), item);
-                    File.WriteAllText(outpuFile, text);
-                    Console.WriteLine(outpuFile);
+                    action.Invoke(path, outputPath, sourcePath, mustacher, item);
                 }
             }
+        }
+
+        private static void CreatePath(string path, string outputPath, string sourcePath, IMustacheRenderizer mustacher, JToken item)
+        {
+            var destPath = mustacher.Render(path.Replace(sourcePath, outputPath, StringComparison.CurrentCultureIgnoreCase), item);
+            if (!Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+        }
+
+        private static void TransformFile(string file, string outputPath, string sourcePath, IMustacheRenderizer mustacher, JToken item)
+        {
+            var outpuFile = mustacher.Render(file.Replace(sourcePath, outputPath, StringComparison.CurrentCultureIgnoreCase), item);
+            var text = mustacher.Render(File.ReadAllText(file), item);
+            File.WriteAllText(outpuFile, text);
+            Console.WriteLine(outpuFile);
         }
     }
 }
