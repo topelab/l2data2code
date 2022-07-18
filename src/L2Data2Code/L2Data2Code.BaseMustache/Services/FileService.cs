7517e87890cd953715b4b5e901fffb6aa2bc6017
topelab <joan@lopezlopez.cat>
2022-07-18 22:43:02 +0200

    Partial version 2.11.2

--- src/L2Data2Code/L2Data2Code.BaseMustache/Services/FileService.cs
+++ src/L2Data2Code/L2Data2Code.BaseMustache/Services/FileService.cs
@@ -2,6 +2,7 @@ using L2Data2Code.SharedLib.Extensions;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -10,7 +11,7 @@ namespace L2Data2Code.BaseMustache.Services
     public class FileService : IFileService
     {
         private const string INCLUDE_TEXT = "{{!include ";
-        private readonly Regex includeRegex = new(@"\{\{!include\s+(?<name>[^\}]+)\}\}", RegexOptions.Singleline | RegexOptions.Compiled);
+        private readonly Regex includeRegex = new(@"(?<spaces>\s*)\{\{!include\s+(?<name>[^\}]+)\}\}", RegexOptions.Singleline | RegexOptions.Compiled);
         private readonly Encoding defaultEncoding = new UTF8Encoding(false);
 
         private Encoding encoding;
@@ -66,53 +67,68 @@ namespace L2Data2Code.BaseMustache.Services
         public string ReadWithIncludes(string templateFile, string basePath = null)
             => ReadWithIncludes(templateFile, basePath, null);
 
-        private string ReadWithIncludes(string templateFile, string basePath = null, HashSet<string> includedFiles = null)
+        private string ReadWithIncludes(string templateFile, string basePath = null, Dictionary<string, string> includedFiles = null)
         {
-            includedFiles ??= new HashSet<string>();
-
-            if (includedFiles.Contains(templateFile))
-            {
-                return null;
-            }
-
-            includedFiles.Add(templateFile);
+            includedFiles ??= new Dictionary<string, string>();
 
             basePath ??= Path.GetDirectoryName(templateFile);
             var templateContent = Read(templateFile);
             if (templateContent.Contains(INCLUDE_TEXT))
             {
-                var lines = templateContent.Split('\n');
-                List<string> replacedLines = new();
-                foreach (var line in lines)
+                if (includedFiles.ContainsKey(templateFile))
                 {
-                    if (line.StartsWith(INCLUDE_TEXT))
-                    {
-                        var match = includeRegex.Match(line);
-                        if (match.Success)
-                        {
-                            var file = match.Groups["name"].Value.Replace('/', Path.DirectorySeparatorChar);
-                            var filePath = Path.Combine(basePath, file);
-                            if (file.StartsWith("~\\"))
-                            {
-                                filePath = file.Replace("~", Directory.GetParent(basePath).Parent.FullName);
-                            }
-                            if (File.Exists(filePath))
-                            {
-                                var fileContent = ReadWithIncludes(filePath, basePath, includedFiles);
-                                if (fileContent != null)
-                                {
-                                    replacedLines.Add(fileContent.ReplaceEndOfLine("\n"));
-                                }
-                            }
-                            continue;
-                        }
-                    }
-                    replacedLines.Add(line);
+                    templateContent = includedFiles[templateFile];
                 }
-                templateContent = string.Join("\n", replacedLines);
+                else
+                {
+                    includedFiles.Add(templateFile, templateContent);
+                    templateContent = GetReplacedContent(basePath, includedFiles, templateContent);
+                    includedFiles[templateFile] = templateContent;
+                }
             }
 
             return templateContent;
+        }
+
+        private string GetReplacedContent(string basePath, Dictionary<string, string> includedFiles, string templateContent)
+        {
+            var lines = templateContent.Split('\n');
+            List<string> replacedLines = new();
+            foreach (var line in lines)
+            {
+                if (line.Contains(INCLUDE_TEXT))
+                {
+                    var match = includeRegex.Match(line);
+                    if (match.Success)
+                    {
+                        var file = match.Groups["name"].Value.Replace('/', Path.DirectorySeparatorChar);
+                        var spaces = match.Groups["spaces"].Value;
+                        var filePath = Path.Combine(basePath, file);
+                        if (file.StartsWith("~\\"))
+                        {
+                            filePath = file.Replace("~", Directory.GetParent(basePath).Parent.FullName);
+                        }
+                        if (File.Exists(filePath))
+                        {
+                            var fileContent = ReadWithIncludes(filePath, basePath, includedFiles);
+                            if (fileContent != null)
+                            {
+                                replacedLines.Add(AddIndent(spaces, fileContent));
+                            }
+                        }
+                        continue;
+                    }
+                }
+                replacedLines.Add(line);
+            }
+            templateContent = string.Join("\n", replacedLines);
+            return templateContent;
+        }
+
+        private string AddIndent(string spaces, string text)
+        {
+            var lines = text.ReplaceEndOfLine("\n").Split("\n").Select(l => string.Concat(spaces, l));
+            return string.Join("\n", lines);
         }
     }
 }
