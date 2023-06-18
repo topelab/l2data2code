df82ade918d8e09629daac6a2909166e1e9935d9
topelab <joan@lopezlopez.cat>
2023-06-18 21:43:43 +0200

    Creating project for net maui

---
+++ src/L2Data2Code/L2Data2Code.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace L2Data2Code.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/L2Data2Code/L2Data2Code.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace L2Data2Code.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
